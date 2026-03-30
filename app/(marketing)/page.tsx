06e7a64f20cbdda52c84cc7538b8f3839ee7f7d9
地球玩家 <1641633140@qq.com>
2026-03-30 22:53:06 +0800

    重新提交

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -3,7 +3,7 @@ import FeaturesSection from '@/components/marketing/FeaturesSection'
 import HowItWorksSection from '@/components/marketing/HowItWorksSection'
 import CreatorsSection from '@/components/marketing/CreatorsSection'
 import EconomySection from '@/components/marketing/EconomySection'
-import CTASection from '@/components/marketing/HeroSection'
+import CTASection from '@/components/marketing/CTASection'
 
 export default function HomePage() {
   return (
